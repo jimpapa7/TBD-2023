0e0acaf23859253cd74bf84b938cfa688217b61b
jimpapa7 <36934982+jimpapa7@users.noreply.github.com>
2023-08-02 18:33:29 +0300

    r_star_tree
    r_star_tree familiarity

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "r_star_tree.hpp"
 using namespace std;
 //Ένα πρόγραμμα C++ αρχίζει με τη main()
 int main ()
 {
-cout<<"C++ is power programming";
-return 0;
+    Rectangle *x=new Rectangle(2);
+    Rectangle *y=new Rectangle(1);
+    Rectangle *z=new Rectangle(3);
+    x->children[0]=*y;
+    cout<<x->children[0].id<<endl;
+    return 0;
 }
